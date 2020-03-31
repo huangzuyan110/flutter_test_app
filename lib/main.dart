14afd14b1093ac403ec9692d5031f4c127e95d3f
黄祖燕 <huangzy@ligeit.com>
2020-03-31 11:34:53 +0800

    删除自带的计数器页面，重新修改

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'cart_page.dart';
+import 'home_page.dart';
+import 'personal_center_page.dart';
+import 'product_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,68 +50,198 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '测试项目',
+      theme: ThemeData(
+        primaryColor: Color(0xFFF43333),
+      ),
+      home: MainScreen()
+    );
+  }
+}
+
+
+class MainScreen extends StatefulWidget {
+  MainScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
+
+  int _selected = 0;
+  
+  // 显示直播间按钮， 默认true:显示
+  bool isShowLiveBotton = true;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  void _onItemTap(int index) {
+    if (_selected != index) {
+      setState(() {
+        debugPrint('select tab $index');
+        _selected = index;
+      });
+    }
+  }
+
+  Widget _selectPage(){
+    switch(_selected) {
+      case 0:
+        return HomePage();
+      case 1:
+       return ProductPage();
+      case 2:
+        return CartPage();
+      case 3:
+        return PersonalCenterPage();
+    }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+    return  Scaffold(
+        body: _selectPage(),
+        bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        unselectedItemColor: Colors.black54,
+        selectedItemColor: Colors.deepOrange,
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTap,
+        currentIndex: _selected,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('首页')),
+          BottomNavigationBarItem(icon: Icon(Icons.shop), title: Text('产品')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), title: Text('购物车')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.supervised_user_circle), title: Text('我的')),
+        ],
+      )
     );
   }
 }
+
+/* class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '团巴拉',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: TblApp(),
+    );
+  }
+}
+
+class TblApp extends StatefulWidget {
+  TblApp({Key key}) : super(key: key);
+
+  _TblAppState createState() => _TblAppState();
+}
+
+class _TblAppState extends State<TblApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ddd"),
+      ),
+      body: Container(
+        child: Scaffold(
+          appBar: AppBar(title: Text("dd22"),),
+          body: Container(
+            child: Text("内容22"),
+          )
+        ),
+      ),
+    );
+  }
+}
+
+
+class TblApp2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "团巴啦",
+      // debugShowCheckedModeBanner:false, //隐藏debug
+      home: Scaffold(
+          appBar: AppBar(
+            leading: new IconButton(
+              icon: Image.asset("images/shoppingcar+sel@2x.png"),
+              tooltip: 'Navigation menu',
+              onPressed: null,
+            ),
+            title: Column(
+              children: <Widget>[
+                Container(
+                  height: 30,
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                colors: <Color>[
+                  Color(0xFFFF7B90),
+                  Color.fromRGBO(254, 69, 69, 1),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              )),
+            ),
+            actions: <Widget>[
+              new IconButton(
+                icon: new Icon(Icons.search),
+                tooltip: 'Search',
+                onPressed: null,
+              ),
+            ],
+          ),
+          body: Container(
+            height: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('左'),
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(color: Colors.red),
+                    child: Text('sdasd'),
+                  ),
+                ),
+                Text('右11'),
+              ],
+            ),
+          )),
+    );
+  }
+}
+ */
+
+
+
